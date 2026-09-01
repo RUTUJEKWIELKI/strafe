ac34653c45ed98a7fb75aaeb3640878f1eb45ef6
RUTUJEKWIELKI <rutujek@gmail.com>
2026-09-01 13:19:25 +0200

    chore: configure workspace quality checks

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+import js from '@eslint/js'
+import prettier from 'eslint-config-prettier'
+import solid from 'eslint-plugin-solid'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  {
+    ignores: [
+      '**/coverage/**',
+      '**/dist/**',
+      '**/node_modules/**',
+      '**/target/**',
+      'apps/web/src/lib/api/schema.d.ts',
+    ],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['apps/web/src/**/*.tsx'],
+    plugins: {
+      solid,
+    },
+    rules: solid.configs.typescript.rules,
+  },
+  prettier,
+)
